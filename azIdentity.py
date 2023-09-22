8c253b01f0ac14609815bed2bdcf177fdf8af1d5
Ovidiu Borlean <ovidiu.borlean@gmail.com>
2023-09-22 16:36:05 +0300

    Update azIdentity.py

--- azIdentity.py
+++ azIdentity.py
@@ -40,12 +40,12 @@ import kubernetes.client
 from azure.identity import ManagedIdentityCredential
 
 
-client = ManagedIdentityCredential(client_id="5a773faa-ce14-4364-bf19-3feb2533cc34")
+client = ManagedIdentityCredential(client_id="")
 scope = "https://management.azure.com/.default"
 token = client.get_token(scope)
-mytoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tIiwiaXN>
+mytoken = ""
 #print(token)
-#credentials = client.get_client_secret("5a773faa-ce14-4364-bf19-3feb2533cc34")
+#credentials = client.get_client_secret("")
 # Deploy the workloadimport kubernetes
 clientk8s = kubernetes.client.CoreV1Api(mytoken)
 print(clientk8s)
