e3937425e0c7420ce0eb70ec8a363f096997bfb6
Ovidiu Borlean <ovidiu.borlean@gmail.com>
2024-03-11 13:47:20 +0200

    Update deploy.sh

--- appconfig/deploy.sh
+++ appconfig/deploy.sh
@@ -6,17 +6,21 @@ AKS_CLUSTER_NAME="aksappconfig"
 UAMI="appconfigmsi"
 SUBSCRIPTION=""
 LOCATION="EastUS"
-APPCFG_NAME="appconfig0001z"
+APPCFG_NAME="appconfig0001zx00"
 
 az group create -n $RESOURCE_GROUP --location $LOCATION
 
 echo "--->Create App Configuration"
 az appconfig create --location $LOCATION --name $APPCFG_NAME --resource-group $RESOURCE_GROUP --enable-public-network true --sku Free
 
-APPCFG_ID=$(az appconfig show -n $APPCFG_NAME -g $RESOURCE_GROUP --query 'id' -otsv) 
+echo "Getting AppConfig Id"
+APPCFG_ID=$(az appconfig show -n $APPCFG_NAME -g $RESOURCE_GROUP --query 'id' -otsv)
+
+echo "Getting AppConfig Endpoint"
+APPCFG_ENDPOINT=$(az appconfig show -n $APPCFG_NAME -g $RESOURCE_GROUP --query endpoint -o tsv)
 
 echo "--->Create AKS Cluster"
-az aks create -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --node-count 2 --network-plugin azure --network-policy calico --enable-workload-identity --enable-oidc-issuer 
+az aks create -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --node-count 2 --network-plugin azure --network-policy calico --enable-workload-identity --enable-oidc-issuer
 
 echo "--->Create a Managed Identity to be used by AppConfig provider"
 az identity create --name $UAMI --resource-group $RESOURCE_GROUP
@@ -29,7 +33,32 @@ echo $AKS_OIDC_ISSUER
 
 echo "--->Create Federated Identity"
 az identity federated-credential create --resource-group $RESOURCE_GROUP --name apconfig_federated --identity-name $UAMI --issuer ${AKS_OIDC_ISSUER} --subject  system:serviceaccount:azappconfig-system:az-appconfig-k8s-provider --audience api://AzureADTokenExchange
- 
-az role assignment create --assignee "${USER_ASSIGNED_IDENTITY_CLIENT_ID}" --scope $APPCFG_ID --role "App Configuration Data Reader" 
- 
+
+
+echo "Creating Role Assignments"
+az role assignment create --assignee $USER_ASSIGNED_IDENTITY_CLIENT_ID --scope $APPCFG_ID --role "App Configuration Data Reader"
+
+echo "AKS Get Credentials"
 az aks get-credentials -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --overwrite
+
+echo "Installing AppConfig Plugin"
+helm install azureappconfiguration.kubernetesprovider oci://mcr.microsoft.com/azure-app-configuration/helmchart/kubernetes-provider --namespace azappconfig-system --create-namespace
+
+cat << EOF > ./appconfig.yaml
+apiVersion: azconfig.io/v1
+kind: AzureAppConfigurationProvider
+metadata:
+  name: appconfigurationprovider-sample
+spec:
+  endpoint: $APPCFG_ENDPOINT
+  target:
+    configMapName: appconfigmap
+    configMapData:
+      type: json
+      key: mysettings.json
+  auth:
+    workloadIdentity:
+      managedIdentityClientId: $USER_ASSIGNED_IDENTITY_CLIENT_ID
+EOF
+
+kubectl apply -f ./appconfig.yaml
